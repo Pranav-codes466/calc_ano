4b54f41fb3cc1ced450f9f7e05cff9b011fb8883
srivathsa2005 <kotrike.srivathsa@s.amity.edu>
2025-07-02 10:01:46 +0530

    addition of two num

---
+++ calc.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+	float num1,num2;
+	float result;
+	printf("enter the first number:");
+	scanf("%f",&num1);
+	printf("enter the second number:");
+	scanf("%f",&num2);
+	result=num1+num2;
+	printf("result of %f and %f is %2f",num1,num2,res);
+
+	return 0;
