eb88afd7376106b5e03776cd5cb4783babe52b73
Pranav Manjunath <pranav.manjunath@s.amity.edu>
2025-07-28 11:23:34 +0530

    spaces removed

--- calc.c
+++ calc.c
@@ -8,24 +8,18 @@ int main()
 	scanf("%f",&num2);
 	result=num1+num2;
 	printf("result of %f + %f is %2f",num1,num2,res);
-       
-        #sub
 	printf("enter the first number:");
 	scanf("%f",&num1);
 	printf("enter the first number:");
 	scanf("%f",&num2);
 	result=num1-num2;
 	printf("result of %f - %f is %2f",num1,num2,res);
-     
-	 #mul
 	printf("enter the first number:");
 	scanf("%f",&num1);
 	printf("enter the second number:");
 	scanf("%f",&num2);
 	result=num1*num2;
 	printf("result of  %f * %f is %2f",num1,num2,res);
-
-	 #div
 	printf("enter the first number:");
 	scanf("%f",&num1);
 	printf("enter the second number:");
